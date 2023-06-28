ca9b9779918e452e4965a6a5298f0791569fff62
Jamie <tidusjar@gmail.com>
2023-06-28 14:27:48 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+  var skills = ["HTML", "CSS", "JS", "React"];
+  var member = {
+    name: "John",
+    age: 23,
+    skills: skills,
+  };
+  // Only change code below this line.
+  return member;
+  // Only change code above this line.
+}
